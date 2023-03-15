1fa83fb66faa9ef005334580d0b15e9e11d65f68
cmstiver <chayse.stiver@gmail.com>
2023-03-14 17:29:20 -0700

    created models

--- backend/backend/twitter_clone/models.py
+++ backend/backend/twitter_clone/models.py
@@ -12,3 +12,73 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+class Tweet(models.Model):
+    content = models.TextField(max_length=280)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content
+
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tweet = models.ForeignKey(Tweet, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('user', 'tweet')
+
+    def __str__(self):
+        return f"{self.user.username} likes {self.tweet.content}"
+
+
+class Retweet(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tweet = models.ForeignKey(Tweet, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'tweet')
+
+    def __str__(self):
+        return f"{self.user.username} retweeted {self.tweet.content}"
+
+
+class Comment(models.Model):
+    content = models.TextField(max_length=280)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tweet = models.ForeignKey(Tweet, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content
+
+
+class Follow(models.Model):
+    follower = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='following')
+    followed = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='followers')
+
+    class Meta:
+        unique_together = ('follower', 'followed')
+
+    def __str__(self):
+        return f"{self.follower.username} follows {self.followed.username}"
+
+
+class Notification(models.Model):
+    recipient = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='notifications')
+    sender = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, related_name='+')
+    tweet = models.ForeignKey(Tweet, on_delete=models.SET_NULL, null=True)
+    type = models.CharField(max_length=20, choices=[(
+        'like', 'Like'), ('retweet', 'Retweet'), ('comment', 'Comment'), ('follow', 'Follow')])
+    timestamp = models.DateTimeField(auto_now_add=True)
+    seen = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.sender.username} {self.type}ed {self.recipient.username}"
