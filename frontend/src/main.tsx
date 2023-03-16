794a58ba336d80e6ea00785b879cf418c5485d38
cmstiver <chayse.stiver@gmail.com>
2023-03-15 22:15:26 -0700

    recreated vite project

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,19 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
-import App from './App';
-import GlobalStyles from './styles/GlobalStyles';
-import theme from './styles/theme';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <GlobalStyles />
-        <App />
-      </ThemeProvider>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
-  document.getElementById('root')
-);
+)
