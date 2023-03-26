8b245d90e179d50f8fdd8a17ba75d9354e7f7f3c
cmstiver <chayse.stiver@gmail.com>
2023-03-25 21:54:12 -0700

    switched around urls

--- backend/backend/twitter_clone/urls.py
+++ backend/backend/twitter_clone/urls.py
@@ -9,10 +9,11 @@ urlpatterns = [
     path('obtain-auth/', ObtainAuthToken.as_view(), name='obtain-auth'),
     path('profiles/<int:id>/', views.UserRetrieve.as_view(), name='retrieve-user'),
     path('tweets/', views.TweetListCreate.as_view(), name='tweet-list-create'),
-    path('tweets/<int:parent_tweet_id>/reply',
-         views.TweetListCreate.as_view(), name='tweet-create-child'),
     path('tweets/<int:tweet_id>/', views.TweetDetail.as_view(),
          name='tweet-detail'),
+    path('tweets/<int:parent_tweet_id>/reply',
+         views.TweetListCreate.as_view(), name='tweet-create-child'),
+
 
 
 
