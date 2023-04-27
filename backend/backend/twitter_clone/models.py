0cdbe1f8d1394c20765cc84b9a3bdba888fd50d5
cmstiver <chayse.stiver@gmail.com>
2023-04-27 04:34:46 -0700

    changed random image maker

--- backend/backend/twitter_clone/models.py
+++ backend/backend/twitter_clone/models.py
@@ -20,6 +20,8 @@ class Profile(models.Model):
 
     def set_image_from_url(self):
         random_string = random.choices(string.ascii_lowercase)
+        for x in range(10):
+            random_string += random.choices(string.ascii_lowercase)
         url = f'https://api.dicebear.com/6.x/fun-emoji/svg?seed={random_string}'
         response = requests.get(url)
         if response.status_code == 200:
