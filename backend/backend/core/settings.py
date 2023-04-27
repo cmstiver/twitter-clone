9fdcabb87058a4913f134d43780714a5f7c5a5a3
cmstiver <chayse.stiver@gmail.com>
2023-04-27 08:50:05 -0700

    deploy

--- backend/backend/core/settings.py
+++ backend/backend/core/settings.py
@@ -14,7 +14,6 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 import dj_database_url
-import django_heroku
 
 load_dotenv()
 
@@ -144,7 +143,7 @@ USE_TZ = True
 
 STATIC_URL = '/assets/'
 STATICFILES_DIRS = [os.path.join(VITE_APP_DIR, 'dist', 'assets')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
@@ -155,5 +154,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-django_heroku.settings(locals())
