f7fbe8470e204e81a88197eaed798f06448ff51c
cmstiver <chayse.stiver@gmail.com>
2023-04-27 09:49:17 -0700

    edited setting

--- backend/backend/core/settings.py
+++ backend/backend/core/settings.py
@@ -155,4 +155,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-django_on_heroku.settings(locals(), staticfiles=False)
+# django_on_heroku.settings(locals(), staticfiles=False)
