b3f33087f2bde24fb7abf2b5f26d7fe6842feff7
cmstiver <chayse.stiver@gmail.com>
2023-04-27 08:53:02 -0700

    deploy

--- backend/backend/core/settings.py
+++ backend/backend/core/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import django_on_heroku
 from pathlib import Path
 import os
 from dotenv import load_dotenv
@@ -154,3 +155,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_on_heroku.settings(locals())
