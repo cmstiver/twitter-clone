18ab66b5dfe4a711c531545600d916483ce39a5d
cmstiver <chayse.stiver@gmail.com>
2023-04-27 06:40:00 -0700

    deploy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import legacy from '@vitejs/plugin-legacy';
 
+
 export default {
   plugins: [react(), legacy()],
 };
