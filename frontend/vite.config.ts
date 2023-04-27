be899f07134b43cc7e974c7b140e93fe765996e7
cmstiver <chayse.stiver@gmail.com>
2023-04-27 05:39:42 -0700

    Installed vite plugins

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import legacy from '@vitejs/plugin-legacy';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default {
+  plugins: [react(), legacy()],
+};
