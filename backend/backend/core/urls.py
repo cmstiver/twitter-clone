2f5dc0196e4d37b1a4b2e9811a843f2dba63332a
cmstiver <chayse.stiver@gmail.com>
2023-04-27 08:43:48 -0700

    deploy

--- backend/backend/core/urls.py
+++ backend/backend/core/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('api/', include('twitter_clone.urls'))
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
